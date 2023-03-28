3d6dfec5e71e729f4bd84044941b4b023c49d6e6
xWesleyMartins <wesley_869@hotmail.com.br>
2023-03-27 22:30:41 -0300

    feat: implementando função "get_unique_industries" no aquivo src/insights/industries.py, completando requisito-03

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,17 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    try:
+        result_data_file = read(path)
+        all_industries = set()
+        for row in result_data_file:
+            if row.get("industry"):
+                all_industries.add(row["industry"])
+        return list(all_industries)
+    except FileNotFoundError:
+        print(f"file {path} Not found")
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
