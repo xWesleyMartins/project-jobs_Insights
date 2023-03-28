87a0957fa3533ead6c41febb5528d154ce264c20
xWesleyMartins <wesley_869@hotmail.com.br>
2023-03-27 21:05:45 -0300

    feat: implementado função "get_unique_job_types" no arquivo "src/insights/jobs.py", completando requisito 02

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -8,41 +8,32 @@ def read(path: str) -> List[Dict]:
     try:
         with open(path) as csv_file:
             return list(csv.DictReader(csv_file))
-    except FileExistsError:
+    except FileNotFoundError:
         print(f"file {path} Not found")
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    try:
+        result_data_file = read(path)
+        return {result_data["job_type"] for result_data in result_data_file}
+    except FileNotFoundError:
+        print(f"file {path} Not found")
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    # """Filters a list of jobs by job_type
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
+    # Parameters
+    # ----------
+    # jobs : list
+    #     List of jobs to be filtered
+    # job_type : str
+    #     Job type for the list filter
+
+    # Returns
+    # -------
+    # list
+    #     List of jobs with provided job_type
+    # """
     raise NotImplementedError
