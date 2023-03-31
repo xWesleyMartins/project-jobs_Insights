fa54aaca9b527de1a8ec1a24f89eadd82b6d7c68
xWesleyMartins <wesley_869@hotmail.com.br>
2023-03-31 13:46:53 -0300

    feat: implementando a função de testes  "test_counter" no arquivo "tests/counter/test_counter.py", completando requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    word_js_count = count_ocurrences("data/jobs.csv", "javascript")
+    word_py_count = count_ocurrences("data/jobs.csv", "Python")
+
+    assert word_js_count == 122
+    assert word_py_count == 1639
